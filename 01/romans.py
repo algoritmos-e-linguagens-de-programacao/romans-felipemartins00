b9552fb5e9f9af3ff1b0bbf5890064c7673cd9f1
Felipe Martins <161171083+felipemartins00@users.noreply.github.com>
2024-06-12 20:18:35 -0300

    Update romans.py

--- 01/romans.py
+++ 01/romans.py
@@ -13,7 +13,15 @@ def int_to_roman(num):
 
 
    def roman_to_int(s):
-      roman_to_int_map = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 } 
+      roman_to_int_map = { 
+      'I': 1,
+      'V': 5, 
+      'X': 10, 
+      'L': 50, 
+      'C': 100,
+      'D': 500, 
+      'M': 1000 
+      } 
       total = 0 
       prev_value = 0
       for char in reversed(s):
